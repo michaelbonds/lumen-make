65a812958d49b12ff10e92cba231894ea9978f56
Michael Bonds <mbonds1219@gmail.com>
2016-01-20 16:41:00 -0600

    cleaned up a little bit

--- src/LumenMakeServiceProvider.php
+++ src/LumenMakeServiceProvider.php
@@ -15,20 +15,11 @@ class LumenMakeServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->registerNewCommand('job', JobMakeCommand::class);
-        $this->registerNewCommand('console', ConsoleMakeCommand::class);
-        $this->registerNewCommand('controller', ControllerMakeCommand::class);
-        $this->registerNewCommand('model', ModelMakeCommand::class);
-        $this->registerNewCommand('middleware', MiddlewareMakeCommand::class);
-        $this->registerNewCommand('exception', ExceptionMakeCommand::class);
-    }
-
-    private function registerNewCommand($name = '', $class = '')
-    {
-        $this->app->singleton('michaelb.lumen-make.'.$name, function ($app) use ($class) {
-            return $app[$class];
-        });
-
-        $this->commands('michaelb.lumen-make.'.$name);
+        $this->commands(JobMakeCommand::class);
+        $this->commands(ConsoleMakeCommand::class);
+        $this->commands(ControllerMakeCommand::class);
+        $this->commands(ModelMakeCommand::class);
+        $this->commands(MiddlewareMakeCommand::class);
+        $this->commands(ExceptionMakeCommand::class);
     }
 }
