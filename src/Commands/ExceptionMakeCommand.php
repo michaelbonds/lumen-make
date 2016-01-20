74d19ac24691f8ceadec3fe3aa1b3c1a8c3fd6e3
Michael Bonds <mbonds1219@gmail.com>
2016-01-20 15:42:03 -0600

    add exception make command

---
+++ src/Commands/ExceptionMakeCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace MichaelB\LumenMake\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+
+class ExceptionMakeCommand extends GeneratorCommand
+{
+    /**
+     * @var string
+     */
+    protected $name = 'make:exception';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Make a new application exception.';
+
+    protected $type = 'Exception';
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return __DIR__.'stubs/exception.stub';
+    }
+
+    protected function getDefaultNamespace($namespace)
+    {
+        return $namespace.'\Exceptions';
+    }
+}
--- src/LumenMakeServiceProvider.php
+++ src/LumenMakeServiceProvider.php
@@ -8,6 +8,7 @@ use MichaelB\LumenMake\Commands\ConsoleMakeCommand;
 use MichaelB\LumenMake\Commands\ControllerMakeCommand;
 use MichaelB\LumenMake\Commands\ModelMakeCommand;
 use MichaelB\LumenMake\Commands\MiddlewareMakeCommand;
+use MichaelB\LumenMake\Commands\ExceptionMakeCommand;
 
 
 class LumenMakeServiceProvider extends ServiceProvider
@@ -19,6 +20,7 @@ class LumenMakeServiceProvider extends ServiceProvider
         $this->registerNewCommand('controller', ControllerMakeCommand::class);
         $this->registerNewCommand('model', ModelMakeCommand::class);
         $this->registerNewCommand('middleware', MiddlewareMakeCommand::class);
+        $this->registerNewCommand('exception', ExceptionMakeCommand::class);
     }
 
     private function registerNewCommand($name = '', $class = '')
