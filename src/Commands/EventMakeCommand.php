396020cec0c1449de15749b10527f402db30da76
Michael Bonds <mbonds1219@gmail.com>
2016-01-21 08:47:38 -0600

    add event make command

---
+++ src/Commands/EventMakeCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace MichaelB\LumenMake\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+
+class EventMakeCommand extends GeneratorCommand
+{
+    /**
+     * @var string
+     */
+    protected $name = 'make:event';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Make a new Event class.';
+
+    protected $type = 'Event';
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return __DIR__.'/stubs/event.stub';
+    }
+
+    protected function getDefaultNamespace($namespace)
+    {
+        return $namespace.'\Events';
+    }
+}
--- src/LumenMakeServiceProvider.php
+++ src/LumenMakeServiceProvider.php
@@ -9,6 +9,7 @@ use MichaelB\LumenMake\Commands\ControllerMakeCommand;
 use MichaelB\LumenMake\Commands\ModelMakeCommand;
 use MichaelB\LumenMake\Commands\MiddlewareMakeCommand;
 use MichaelB\LumenMake\Commands\ExceptionMakeCommand;
+use MichaelB\LumenMake\Commands\EventMakeCommand;
 
 
 class LumenMakeServiceProvider extends ServiceProvider
@@ -21,5 +22,6 @@ class LumenMakeServiceProvider extends ServiceProvider
         $this->commands(ModelMakeCommand::class);
         $this->commands(MiddlewareMakeCommand::class);
         $this->commands(ExceptionMakeCommand::class);
+        $this->commands(EventMakeCommand::class);
     }
 }
